5f5689ef3593ed977f5e1537a534bf9bc5636bde
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-07-21 19:08:01 -0300

    dashboard centralizada

--- src/routes/dashboard/index@logged.svelte
+++ src/routes/dashboard/index@logged.svelte
@@ -19,52 +19,54 @@
 
 <div class="products-list">
   <h2 class="mb-4 mt-2">Produtos</h2>
-  <div
-    class="d-flex align-items-center flex-wrap justify-content-right center-mobile"
-  >
-    {#each products as product}
-      <div
-        class="card"
-        style="width: auto;margin-right: 25px;margin-bottom:15px;"
-      >
-        {#if product.images_display.length > 0}
-          <img
-            src={product.images_display[0].image}
-            alt={product.name}
-            width="300"
-            height="287"
-            style="object-fit: cover;"
-          />
-        {:else}
-          Sem imagem
-        {/if}
-        <div class="card-body text-center w-100">
-          <h5 class="card-title">
-            <a href="/products/{product.id}">{product.name}</a>
-          </h5>
-          {#if product.owner_display.address}
-            <p class="card-text">
-              <small class="text-muted"
-                >{product.owner_display.address.city}, {product.owner_display
-                  .address.district}</small
-              >
-            </p>
+  <div class="w-100 d-flex align-items-center flex-wrap justify-content-center">
+    <div
+      class="d-flex align-items-center flex-wrap justify-content-center center-mobile"
+    >
+      {#each products as product}
+        <div
+          class="card"
+          style="width: auto;margin-right: 25px;margin-bottom:15px;"
+        >
+          {#if product.images_display.length > 0}
+            <img
+              src={product.images_display[0].image}
+              alt={product.name}
+              width="300"
+              height="287"
+              style="object-fit: cover;"
+            />
+          {:else}
+            Sem imagem
           {/if}
-          <div class="d-flex align-items-center justify-content-around">
-            <a
-              href="mailto:{product.owner_display.email}"
-              class="btn btn-bazar email">Enviar Email</a
-            >
-            <a href="/products/{product.id}/" class="btn btn-primary"
-              >Detalhes</a
-            >
+          <div class="card-body text-center w-100">
+            <h5 class="card-title">
+              <a href="/products/{product.id}">{product.name}</a>
+            </h5>
+            {#if product.owner_display.address}
+              <p class="card-text">
+                <small class="text-muted"
+                  >{product.owner_display.address.city}, {product.owner_display
+                    .address.district}</small
+                >
+              </p>
+            {/if}
+            <div class="d-flex align-items-center justify-content-around">
+              <a
+                href="mailto:{product.owner_display.email}"
+                class="btn btn-bazar email">Enviar Email</a
+              >
+              <a href="/products/{product.id}/" class="btn btn-primary"
+                >Detalhes</a
+              >
+            </div>
+          </div>
+          <div class="card-footer text-muted text-center">
+            {formatDate(product.created_at)}
           </div>
         </div>
-        <div class="card-footer text-muted text-center">
-          {formatDate(product.created_at)}
-        </div>
-      </div>
-    {/each}
+      {/each}
+    </div>
   </div>
 </div>
 
